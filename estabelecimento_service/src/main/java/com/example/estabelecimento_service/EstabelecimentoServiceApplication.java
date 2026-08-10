941fa32baba48366a4bdc63a24b03e583d4f3579
lourenço <lourencomadoro912@gmail.com>
2026-08-10 18:28:38 +0200

    First commit of my new project

---
+++ estabelecimento_service/src/main/java/com/example/estabelecimento_service/EstabelecimentoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.estabelecimento_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstabelecimentoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstabelecimentoServiceApplication.class, args);
+	}
+
+}
---
+++ estabelecimento_service/src/test/java/com/example/estabelecimento_service/EstabelecimentoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.estabelecimento_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstabelecimentoServiceApplicationTests {
+
+	//@Test
+	//void contextLoads() {
+	//}
+
+}
