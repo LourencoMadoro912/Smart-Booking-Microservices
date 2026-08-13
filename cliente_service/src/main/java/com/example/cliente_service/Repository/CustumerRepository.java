522f396a45f1b8b02b091ffc0435ce9381ec788f
lourenco madoro <lourencomadoro912@gmail.com>
2026-08-13 08:29:14 +0200

    rename Repository interface to CustomerRepository

--- cliente_service/src/main/java/com/example/cliente_service/Repository/CustumerRepository.java
+++ cliente_service/src/main/java/com/example/cliente_service/Repository/CustumerRepository.java
@@ -3,6 +3,6 @@ package com.example.cliente_service.Repository;
 import com.example.cliente_service.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Repository extends JpaRepository<Long, Customer> {
+public interface CustumerRepository extends JpaRepository<Long, Customer> {
     boolean existeByEmail(String email);
 }
