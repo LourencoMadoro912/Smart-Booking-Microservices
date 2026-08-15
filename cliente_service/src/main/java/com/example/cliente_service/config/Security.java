df0f6b7333df5e0eae14fc3d651ace65eb11b9bd
lourenco madoro <lourencomadoro912@gmail.com>
2026-08-15 17:25:16 +0200

    feat(config) add AuthenticationManager to our security

--- cliente_service/src/main/java/com/example/cliente_service/config/Security.java
+++ cliente_service/src/main/java/com/example/cliente_service/config/Security.java
@@ -3,6 +3,8 @@ package com.example.cliente_service.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -30,5 +32,10 @@ public class Security {
                         .anyRequest().authenticated()
                 )
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+        return config.getAuthenticationManager();
     }
 }
