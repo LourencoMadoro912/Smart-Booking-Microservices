bc84be8ac411e02404faa8265d6e3bd733668fec
lourenco madoro <lourencomadoro912@gmail.com>
2026-08-16 16:22:44 +0200

    feat(auth) implement jwtFilter, to filter token

---
+++ cliente_service/src/main/java/com/example/cliente_service/auth/JwtFilter.java
@@ -0,0 +1,58 @@
+package com.example.cliente_service.auth;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private  final CustomUserDetailsService customUserDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String authHeader=request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        String token=authHeader.substring(6);
+
+        try {
+            Long userId = jwtService.extractUserId(token);
+            String email = jwtService.extractEmail(token);
+
+            if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserPrincipal userDetails = (UserPrincipal) customUserDetailsService.loadUserByUsername(email);
+
+                if (jwtService.isTokenValid(token, userDetails)) {
+                    UsernamePasswordAuthenticationToken authToken =
+                            new UsernamePasswordAuthenticationToken(
+                                    userDetails,
+                                    null,
+                                    userDetails.getAuthorities()
+                            );
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            }
+        } catch (Exception e) {
+            // opcional: logar erro
+        }
+}
+
+
+
+    }
